10a76ff05fecae41147dc9ced7768de2315a2714
Douglas Terra Machado <101201799+douglasterra@users.noreply.github.com>
2025-05-22 15:16:43 -0300

    Delete R/zzz.R

--- R/zzz.R
+++
@@ -1,8 +0,0 @@
-# zzz.R
-
-# Supress global variable notes in R CMD check
-utils::globalVariables(c(
-  "codigo_genoma", "consensus_name_this_study", "presenca",
-  "rf_model", "svm_model", "meta_model",
-  "Preferred_name", "KEGG_ko", "spoiiie_ko_eccca"
-))
